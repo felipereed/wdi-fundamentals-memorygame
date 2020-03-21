d8942b9e7afd10709c5fd0e74292d0c1fd53b974
Felipe Reed <Felipe@Users-Air.fios-router.home>
2020-03-20 22:40:01 -0400

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,12 +1,6 @@
 let cards = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
+function checkForMatch (){
 	if(cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
 	} 
@@ -14,3 +8,12 @@ if (cardsInPlay.length === 2) {
 		alert("Sorry, try again.");
 	}
 }
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+flipCard(0);
+flipCard(2);
